818458e53b87f66bbc8f3ba3c6abbfe4513da86b
SameerCD30 <sameer.cd30@gmail.com>
2026-08-07 15:26:15 +0530

    initialized the project

---
+++ railway_api_spring_boot/src/main/java/com/Sameer/railway_api_spring_boot/RailwayApiSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.Sameer.railway_api_spring_boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RailwayApiSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RailwayApiSpringBootApplication.class, args);
+	}
+
+}
---
+++ railway_api_spring_boot/src/test/java/com/Sameer/railway_api_spring_boot/RailwayApiSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Sameer.railway_api_spring_boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RailwayApiSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
